101369ed2160c74ad14c611e3960052b76ab20ca
root <root@gmail.com>
2022-12-14 23:32:00 +0900

    feat: cf parameter

--- main.cpp
+++ main.cpp
@@ -17,37 +17,45 @@ int main(int argc, char **argv) {
     exit(1);
   }
 
-  list<string> ssidList;
+  list<string> ssidList {
+    "aaaaaaaaaaaaa",
+  };
   
-  ifstream infile(argv[2]);
-  string line;
-  for(string line; getline(infile, line);) {
-    ssidList.push_back(line);
-  }
-  infile.close();
+  // ifstream infile(argv[2]);
+  // string line;
+  // for(string line; getline(infile, line);) {
+  //   ssidList.push_back(line);
+  // }
+  // infile.close();
   
   for (string ssid: ssidList) {
     cout << ssid << '\n';
   }
+
+
 
   list<string>::iterator it = ssidList.begin();
 
   while (true) {
     RadioTap tap;
 
-    Dot11::address_type ap        = "00:11:22:33:44:55";
+    Dot11::address_type ap        = "f9:cc:c2:f4:06:99";
     Dot11::address_type broadcast = "ff:ff:ff:ff:ff:ff";
     Dot11Beacon beacon(broadcast, ap);
-    beacon.addr4(ap);
+    beacon.addr2(ap);
+    beacon.addr3(ap);
     beacon.ssid(*it);
-    beacon.ds_parameter_set(1);
-    beacon.supported_rates({ 1.0f, 5.5f, 11.0f });
+    beacon.supported_rates({ 1.0f, 2.0f, 5.5f, 11.0f });
+    beacon.ds_parameter_set(3);
+    beacon.cf_parameter_set({(uint8_t)1, (uint8_t)2, (uint16_t) 0, (uint16_t) 0});
     tap.inner_pdu(beacon);
-
+    
     PacketSender sender(argv[1]);
     sender.send(tap);
-    usleep(1000);
 
+    break;
+
+    usleep(10000);
     if (++it == ssidList.end())
       it = ssidList.begin();
   }
